d543af808bd977ea4d95cd08a90cdb445f0673b2
LuckedCoronet <lc45@lc-studios.net>
2025-10-28 20:36:43 +0900

    refactor(build): error handling

--- src/build/build.ts
+++ src/build/build.ts
@@ -1,8 +1,11 @@
 import type { ConfigInput } from "@/config/config-input-types";
 import type { Config } from "@/config/config-parsed-types";
 import { parseConfig } from "@/config/parsing";
+import { styleText } from "node:util";
 
-const _build = async (config: Config, signal?: AbortSignal): Promise<void> => {};
+const buildOperation = async (config: Config, signal?: AbortSignal): Promise<void> => {
+	signal?.throwIfAborted();
+};
 
 /**
  * Builds the project based on the provided configuration.
@@ -12,7 +15,22 @@ const _build = async (config: Config, signal?: AbortSignal): Promise<void> => {}
  * it will not resolve until it's aborted via `signal` (AbortSignal).
  */
 export const build = async (config: ConfigInput, signal?: AbortSignal): Promise<void> => {
-	const parsedConfig = parseConfig(config);
+	if (signal?.aborted) {
+		console.error(styleText("red", "The provided AbortSignal is already aborted."));
+		return;
+	}
 
-	await _build(parsedConfig, signal);
+	let parsedConfig: Config;
+	try {
+		parsedConfig = parseConfig(config);
+	} catch (error) {
+		console.error(styleText("red", `Failed to parse user provided configuration: ${error}`));
+		return;
+	}
+
+	try {
+		await buildOperation(parsedConfig, signal);
+	} catch (error) {
+		console.error(styleText("red", `Build failed: ${error}`));
+	}
 };
