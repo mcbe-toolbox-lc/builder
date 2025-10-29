3acc32ced1e026dece68f5840cad81df10f7c31d
LuckedCoronet <lc45@lc-studios.net>
2025-10-29 10:13:19 +0900

    test(build): add build-context.test.ts

---
+++ test/build/build-context.test.ts
@@ -0,0 +1,31 @@
+import { resolveAndValidateUserConfig } from "@/build/build-context";
+import path from "node:path";
+import { describe, expect, it } from "vitest";
+
+describe("resolveAndValidateUserConfig()", () => {
+	it("throws if no packs are defined", () => {
+		expect(() => resolveAndValidateUserConfig({})).toThrowError();
+	});
+
+	it("resolves targetDir correctly", () => {
+		const inputBpTargetDir = "bp_target";
+		const inputRpTargetDir1 = "rp_target_1";
+		const inputRpTargetDir2 = "rp_target_2";
+		const expectedBpTargetDirs = [path.resolve(inputBpTargetDir)];
+		const expectedRpTargetDirs = [path.resolve(inputRpTargetDir1), path.resolve(inputRpTargetDir2)];
+
+		const config = resolveAndValidateUserConfig({
+			behaviorPack: {
+				srcDir: "",
+				targetDir: inputBpTargetDir,
+			},
+			resourcePack: {
+				srcDir: "",
+				targetDir: [inputRpTargetDir1, inputRpTargetDir2],
+			},
+		});
+
+		expect(config.bpConfig?.targetDirs).toStrictEqual(expectedBpTargetDirs);
+		expect(config.rpConfig?.targetDirs).toStrictEqual(expectedRpTargetDirs);
+	});
+});
