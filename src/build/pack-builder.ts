a533fdaec13be2e29c4ca2f1b4f95bd6b76ec7ea
LuckedCoronet <lc45@lc-studios.net>
2025-10-30 13:12:39 +0900

    feat(build): add logger to PackBuilder

--- src/build/pack-builder.ts
+++ src/build/pack-builder.ts
@@ -1,8 +1,12 @@
+import type { Logger } from "@/utils/logger";
 import type { PackConfig } from "./build-config";
 import type { BuildExecutionContext } from "./build-system";
 
 export class PackBuilder {
-	constructor(readonly config: PackConfig) {}
+	constructor(
+		readonly config: PackConfig,
+		private readonly logger: Logger,
+	) {}
 
 	async build(ctx: BuildExecutionContext): Promise<void> {}
 }
